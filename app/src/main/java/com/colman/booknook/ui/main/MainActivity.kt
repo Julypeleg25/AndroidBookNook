4228fb751bd7575b00b10321a0a97ece4dd61ec1
July Peleg <julypeleg@gmail.com>
2026-02-20 00:03:09 +0200

    refactor(main): Update MainActivity auth logic

--- app/src/main/java/com/colman/booknook/ui/main/MainActivity.kt
+++ app/src/main/java/com/colman/booknook/ui/main/MainActivity.kt
@@ -2,10 +2,7 @@ package com.colman.booknook.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import com.colman.booknook.R
 import com.colman.booknook.databinding.ActivityMainBinding
-import com.colman.booknook.model.Model
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,17 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
-        val navController = navHostFragment?.navController
-        val bottomNav = binding.bottomNav
-
-        if (navController != null) {
-            androidx.navigation.ui.NavigationUI.setupWithNavController(bottomNav, navController)
-        }
-
-        if (Model.currentUserId() != null) {
-             navController?.navigate(R.id.postsFragment)
-        }
     }
 }
