62eea77a3f1768081d9d073d11db111f2fde5591
July Peleg <julypeleg@gmail.com>
2026-02-20 00:03:07 +0200

    fix(ui): Refactor WishlistFragment and Adapter package

--- app/src/main/java/com/colman/booknook/ui/wishlist/WishlistFragment.kt
+++ app/src/main/java/com/colman/booknook/ui/wishlist/WishlistFragment.kt
@@ -1,7 +1,33 @@
 package com.colman.booknook.ui.wishlist
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.colman.booknook.R
+import com.colman.booknook.databinding.FragmentWishlistBinding
+import com.colman.booknook.model.Model
 
-class WishlistFragment : Fragment(R.layout.fragment_placeholder) {
+class WishlistFragment : Fragment(R.layout.fragment_wishlist) {
+
+    private lateinit var binding: FragmentWishlistBinding
+    private lateinit var adapter: WishlistAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding = FragmentWishlistBinding.bind(view)
+
+        val uid = Model.currentUserId() ?: return
+
+        adapter = WishlistAdapter { item ->
+            Model.removeFromWishlist(uid, item.bookId)
+            Toast.makeText(requireContext(), "Removed", Toast.LENGTH_SHORT).show()
+        }
+        binding.recycler.layoutManager = LinearLayoutManager(requireContext())
+        binding.recycler.adapter = adapter
+
+        Model.observeWishlist(uid).observe(viewLifecycleOwner) { list ->
+            adapter.submit(list)
+        }
+    }
 }
