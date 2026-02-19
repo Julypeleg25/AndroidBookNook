ddf8dde5eb3ee0b12a86e78112aaa8b39206675b
July Peleg <julypeleg@gmail.com>
2026-02-20 00:03:04 +0200

    fix(repo): Move AppLocalRepository and AppDatabase to com.colman.booknook

--- app/src/main/java/com/colman/booknook/data/local/AppDatabase.kt
+++ app/src/main/java/com/colman/booknook/data/local/AppDatabase.kt
@@ -1,14 +1,29 @@
 package com.colman.booknook.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.colman.booknook.data.local.dao.*
 import com.colman.booknook.data.local.entities.*
 
-@Database(entities = [UserEntity::class, PostEntity::class, BookEntity::class, WishlistEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [PostEntity::class, WishlistEntity::class, UserEntity::class, CachedBookEntity::class],
+    version = 1
+)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun userDao(): UserDao
     abstract fun postDao(): PostDao
-    abstract fun bookDao(): BookDao
     abstract fun wishlistDao(): WishlistDao
+    abstract fun userDao(): UserDao
+    abstract fun cachedBookDao(): CachedBookDao
+
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+        fun getInstance(context: Context): AppDatabase =
+            INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "booknook.db")
+                    .build()
+                    .also { INSTANCE = it }
+            }
+    }
 }
