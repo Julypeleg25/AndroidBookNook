ef3bfc198c3088ab51873d0536ab427127a5f137
julypeleg25 <julypeleg@gmail.com>
2026-03-02 15:30:27 +0200

    initliaze firebase

--- app/src/main/java/com/colman/booknook/base/MyApplication.kt
+++ app/src/main/java/com/colman/booknook/base/MyApplication.kt
@@ -1,11 +1,12 @@
 ﻿package com.colman.booknook.base
-
+import com.google.firebase.FirebaseApp
 import android.app.Application
 import com.colman.booknook.model.Model
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        FirebaseApp.initializeApp(this)
         Model.init(this)
     }
 }
