dbe8247722e2428da8ddedf948dfef6b73f0521a
Julypeleg25 <julypeleg@gmail.com>
2026-05-04 22:57:47 +0300

    Clean up the local Room database reset path.

--- app/src/main/java/com/booknook/app/data/local/dao/CommentDao.kt
+++ app/src/main/java/com/booknook/app/data/local/dao/CommentDao.kt
@@ -1,7 +1,11 @@
 package com.booknook.app.data.local.dao
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
 import com.booknook.app.data.local.entities.CommentEntity
 
 @Dao
