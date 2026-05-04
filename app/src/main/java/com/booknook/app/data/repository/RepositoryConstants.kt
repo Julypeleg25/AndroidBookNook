5cf2eb478491f4f318454485bee36932bfa06dff
Julypeleg25 <julypeleg@gmail.com>
2026-05-04 22:58:29 +0300

    Clean up saved-list and media storage flows.

---
+++ app/src/main/java/com/booknook/app/data/repository/RepositoryConstants.kt
@@ -0,0 +1,7 @@
+package com.booknook.app.data.repository
+
+internal object PostRepositoryConstants {
+    const val REFRESH_INTERVAL_MS = 300_000L
+    const val PAGE_SIZE = 15L
+    const val DEFAULT_USERNAME = "User"
+}
---
+++ app/src/main/java/com/booknook/app/model/storage/StorageConstants.kt
@@ -0,0 +1,6 @@
+package com.booknook.app.model.storage
+
+internal object StorageConstants {
+    const val AVATAR_UPLOAD_CONTEXT = "avatar"
+    const val POST_IMAGE_UPLOAD_CONTEXT = "post image"
+}
